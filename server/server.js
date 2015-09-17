b9183384c6da620bc6b8b8952a1204ce81d54311
Karl Lundstig <karl.lundstig@gmail.com>
2015-09-17 20:47:40 +0200

    Testapi, kan lägga till, fråga efter, ändra och ta bort users

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
+var async = require('async');
 
 //små fina funktioner
 function getPrettyTime() {
@@ -23,7 +24,9 @@ function log(text) {
 }
 
 //Databas
+//config.autoIndex = false;
 mongoose.connect('mongodb://127.0.0.1:27017');
+var User = require('./models/user.js');
 
 //låter oss få data från en POST
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -34,9 +37,66 @@ var ip = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 
 //Routes
 var router = express.Router();
+
+router.use(function(req, res, next) {
+    log('Recieved request for ' + req.path);
+    next(); //gå vidare till nästa handler
+});
+
 router.get('/test', function(req,  res) {
     res.status(200).json({ message: 'dumdididumdidum!' });
 });
+
+router.route('/users')
+    .post(function(req, res) {
+        var user = new User();
+        user.name = req.body.name;
+        user.save(function(err) {
+            if (err)
+                res.send(err);
+            res.json({ message: 'User created' });
+        });
+    })
+    .get(function(req, res) {
+        User.find(function(err, users) {
+            if (err)
+                res.send(err);
+            res.json(users);
+        });
+    });
+
+router.route('/users/:id')
+    .get(function(req, res) {
+        User.findById(req.params.id, function(err, user) {
+            if (err)
+                res.send(err);
+            res.json(user);
+        });
+    })
+    .put(function(req, res) {
+        User.findById(req.params.id, function(err, user) {
+            if (err)
+                res.send(err);
+            user.name = req.body.name;
+            user.save(function (err) {
+                if (err)
+                    res.send(err);
+
+                res.json({ message: 'User updated' });
+            });
+        });
+    })
+    .delete(function(req, res) {
+        User.findById(req.params.id, function(err, user) {
+            if (err)
+                res.send(err);
+            user.remove(function (err) {
+                if (err)
+                    res.send(err);
+                res.json({ message: 'User deleted' });
+            });
+        });
+    });
 
 app.use('/', router);
 
